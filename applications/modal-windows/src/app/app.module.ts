b6a66b98135e43f961df8ed87665a01d1b545d4b
Cam Dziurgot <camerondziurgot@gmail.com>
2020-09-26 03:53:38 -0400

    replace navbar with shared header component

--- applications/modal-windows/src/app/app.module.ts
+++ applications/modal-windows/src/app/app.module.ts
@@ -6,17 +6,17 @@ import { WindowModule } from '@progress/kendo-angular-dialog';
 
 import { AppComponent } from './app.component';
 import { ModalWindowComponent } from './modal-window/modal-window.component';
-import { NavBarComponent } from './nav-bar/nav-bar.component';
 
 import { FooterComponent } from '../../../../libraries/footer/src/lib/footer.component';
+import { HeaderComponent } from '../../../../libraries/header/src/lib/header.component';
 import { OverviewTextComponent } from '../../../../libraries/overview-text/src/lib/overview-text.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     FooterComponent,
+    HeaderComponent,
     ModalWindowComponent,
-    NavBarComponent,
     OverviewTextComponent
   ],
   imports: [
