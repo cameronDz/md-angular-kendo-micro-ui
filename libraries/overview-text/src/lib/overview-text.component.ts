26826131c818b692a373ad774c69a9ffc431bbe4
Cam Dziurgot <camerondziurgot@gmail.com>
2020-09-26 01:41:00 -0400

    add component content

--- libraries/overview-text/src/lib/overview-text.component.ts
+++ libraries/overview-text/src/lib/overview-text.component.ts
@@ -1,14 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnChanges, OnInit, SimpleChanges } from '@angular/core';
 
 @Component({
   selector: 'md-overview-text',
   templateUrl: './overview-text.component.html',
   styleUrls: [ './overview-text.component.scss']
 })
-export class OverviewTextComponent implements OnInit {
+export class OverviewTextComponent implements OnChanges, OnInit {
+
+  @Input() title: string = '';
+  @Input() subtitle: string = '';
+  @Input() points: Array<string> = [];
+
+  public hasPoints: boolean = false;
 
   constructor() {}
 
   ngOnInit(): void {}
 
+  ngOnChanges(changes: SimpleChanges): void {
+      this.hasPoints = this.points && !!this.points.length;
+  }
 }
--- libraries/overview-text/src/public-api.ts
+++ libraries/overview-text/src/public-api.ts
@@ -3,4 +3,3 @@
  */
 
 export * from './lib/overview-text.component';
-export * from './lib/overview-text.module';
