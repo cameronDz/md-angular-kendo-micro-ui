42b748eb1df8026077a04386d21ac769c0f059c5
cameronDz <camerondziurgot@gmail.com>
2021-04-19 22:29:47 -0400

    poc using dropdown in grid

--- applications/collections/src/app/components/kendo/kendo-edit-grid/kendo-edit-grid.component.ts
+++ applications/collections/src/app/components/kendo/kendo-edit-grid/kendo-edit-grid.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { GridComponent } from '@progress/kendo-angular-grid';
 import * as _gridData from '../../../../assets/component-data/grid.json';
 
 @Component({
@@ -9,6 +10,17 @@ import * as _gridData from '../../../../assets/component-data/grid.json';
 export class KendoEditGridComponent implements OnInit {
 
   public gridData: any = _gridData.payload;
+  // dropdown data
+  public selectData: Array<{ dataValue: string, displayValue: string }> = [
+    { dataValue: "A", displayValue: "Choice A" },
+    { dataValue: "B", displayValue: "Choice B" },
+    { dataValue: "Dog", displayValue: "Choice Dog" },
+    { dataValue: "", displayValue: "Choice nil" }
+  ];
+
+  private currentId: string = '';
+  public currentValue: string = '';
+
   constructor() {}
 
   ngOnInit(): void {}
@@ -17,11 +29,35 @@ export class KendoEditGridComponent implements OnInit {
     console.log('onStateChange:', event);
   }
 
-  public cellClickHandler(event: any): void {
-    console.log('cellClickHandler:', event);
+  public cellClickHandler(event: GridClickEvent): void {
+    const id: string = event?.dataItem?.id;
+    if (!!id) {
+      this.currentId = id;
+    }
+    if (!!event && !event.isEdited) {
+      event.sender.editCell(event.rowIndex, event.columnIndex);
+    }
   }
 
   public cellCloseHandler(event: any): void {
-    console.log('cellCloseHandler:', event);
+    this.currentId = '';
   }
+
+  public handleGridEvent(event: any): void {
+    const index: number = (!Array.isArray(this.gridData)) ? -1 : this.gridData.findIndex((item: any) => {
+      return ((item) && (item.id) && (item.id === this.currentId));
+    });
+    if ((event?.displayValue) && (index > -1)) {
+      this.gridData[index].name = event.displayValue;
+    }
+  }
+}
+
+class GridClickEvent {
+  constructor(
+    public sender: GridComponent,
+    public rowIndex: number,
+    public columnIndex: number,
+    public dataItem: any,
+    public isEdited: boolean) {}
 }
